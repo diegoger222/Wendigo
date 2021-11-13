83ade247311ee9930cdab169c216caf31e83d9a9
diegoger222 <runyxp@gmail.com>
2021-11-13 12:25:23 +0100

    retroceso arma

--- Assets/Scripts/Arma/DisparoArma.cs
+++ Assets/Scripts/Arma/DisparoArma.cs
@@ -11,27 +11,45 @@ public class DisparoArma : MonoBehaviour
 
     public float shotForce = 1500;
     public float shotRate = 0.5f;
+    public float recoilForce = 4f;
 
     private float shotRateTime = 0;
 
     // Update is called once per frame
-    void Update()
+   
+    private void Update()
+    {
+        Disparo();
+
+        transform.localPosition = Vector3.Lerp(transform.localPosition, Vector3.zero, Time.deltaTime * 5f);
+    }
+    private void Disparo()
     {
         if (Input.GetButtonDown("Fire1"))
         {
             if (Time.time > shotRateTime && pistola.activeSelf)
             {
-               
+                //Vector3 a = transform.localPosition;
+                AddRecoil();
                 GameObject newBullet;
                 newBullet = Instantiate(bullet, spawnPoint.position, spawnPoint.rotation);
                 newBullet.GetComponent<Rigidbody>().AddForce(spawnPoint.forward * shotForce);
                 shotRateTime = Time.time + shotRate;
 
+                
                 Destroy(newBullet, 2);
+
+                
             }
         }
 
         
      
     }
+    private void AddRecoil()
+    {
+        transform.Rotate(-recoilForce, 0f, 0f);
+        transform.position = transform.position - transform.right * (recoilForce / 30f);
+    }
+   
 }
