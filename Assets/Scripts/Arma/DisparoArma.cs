37eec4f5f9aed9714471dd0aa97276ffeae6e541
David Upv <dagarpa4upv@outlook.es>
2021-11-18 23:42:46 +0100

    7 balas con diferentes direcciones

--- Assets/Scripts/Arma/DisparoArma.cs
+++ Assets/Scripts/Arma/DisparoArma.cs
@@ -10,8 +10,13 @@ public class DisparoArma : MonoBehaviour
     public GameObject pistola;
 
     public float shotForce = 1500;
-    public float shotRate = 0.5f;
+    public float shotRate = 0.6f;
     public float recoilForce = 4f;
+
+    public float rechargeRate = 1.3f;
+    public float rechargeRateTime = 0;
+    public int recharging = 0;
+    public int numberOfShots = 2;
 
     private float shotRateTime = 0;
 
@@ -25,21 +30,29 @@ public class DisparoArma : MonoBehaviour
     }
     private void Disparo()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1"))
         {
-            if (Time.time > shotRateTime && pistola.activeSelf)
+            if (recharging < numberOfShots && Time.time > shotRateTime && pistola.activeSelf)
             {
                 //Vector3 a = transform.localPosition;
                 AddRecoil();
                 GameObject newBullet;
-                newBullet = Instantiate(bullet, spawnPoint.position, spawnPoint.rotation);
-                newBullet.GetComponent<Rigidbody>().AddForce(spawnPoint.forward * shotForce);
+
+                for (int i = -3; i < 4; i++)    //Se disparan 7 balas, con diferentes desviaciones
+                {
+                    Vector3 desviacion = new Vector3(i * 0.1f, i%2 * 0.1f, 0);
+                    newBullet = Instantiate(bullet, spawnPoint.position, spawnPoint.rotation);
+                    newBullet.GetComponent<Rigidbody>().AddForce((spawnPoint.forward + desviacion) * shotForce);
+                    Destroy(newBullet, 2);
+                } 
                 shotRateTime = Time.time + shotRate;
+                rechargeRateTime = Time.time + rechargeRate;
+                recharging++;
+            }
 
-                
-                Destroy(newBullet, 2);
-
-                
+            if (recharging == numberOfShots && Time.time > rechargeRateTime)
+            {
+                recharging = 0;
             }
         }
 
